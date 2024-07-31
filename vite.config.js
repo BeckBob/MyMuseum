5508bb9dd7bdd51a35ab325a30dd3412f96d934e
BeckBob <unsworthbecky@gmail.com>
2024-07-31 17:18:22 +0100

    changed vite-config file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [react()],
     root: '.', 
     build: {
-        outDir: 'dist', 
+        outDir: 'build', 
         rollupOptions: {
             input: 'index.html', 
         },
